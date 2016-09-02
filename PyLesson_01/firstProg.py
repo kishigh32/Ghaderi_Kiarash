1ea1ace88af11df7c1d5b82e0ede0e66746dca82
ghaderik0521 <kishigh32@gmail.com>
2016-09-02 10:28:41 -0700

    added files

---
+++ PyLesson_01/firstProg.py
@@ -0,0 +1,5 @@
+print("H   H EEEEE L     L      OOO      W   W  OOO  RRRR  L     DDDD  !!")
+print("H   H E     L     L     O   O     W W W O   O R   R L     D  D  !!")
+print("HHHHH EEEEE L     L     O   O     W W W O   O RRRR  L     D  D  !!")
+print("H   H E     L     L     O   O  ,,  W W  O   O R   R L     D  D")
+print("H   H EEEEE LLLLL LLLLL  OOO  ,,   W W   OOO  R   R LLLLL DDDD  !!")
