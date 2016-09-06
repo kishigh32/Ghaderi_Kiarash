938c69ccd6312fa9d0ac032c88da8db55a6d6e93
ghaderik0521 <kishigh32@gmail.com>
2016-09-06 10:26:11 -0700

    new
    new

---
+++ PyLesson_01/Lab_02.py
@@ -0,0 +1,15 @@
+n1 = 12
+n2 = 7
+print(n1, "multiplied by " ,n2, "is" ,n1 * n2)
+
+line1 = "Kiarash Ghaderi"
+line2 = "1400 Torrey Bluff Dr"
+line3 = "California"
+print(line1)
+print(line2)
+print(line3)
+L = 4
+W = 6
+H = 5
+print("The surface area of your rectangle is" ,L * W * H)
+
---
+++ PyLesson_01/PyLesson_02.py
@@ -0,0 +1,4 @@
+l = 8
+w = 5
+recArea = l * w
+print("The area of your rectangle is", recArea)
