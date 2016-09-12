340dfe050061ab28926267be3a4e97a96e22cd2d
ghaderik0521 <kishigh32@gmail.com>
2016-09-12 09:37:48 -0700

    practice

---
+++ Quiz_01/Quiz_01 Study Guide.py
@@ -0,0 +1,8 @@
+num = 45
+print("IronMan has" ,num, "kinds of weapons in his suit.")
+
+pres = "Abraham Lincoln"
+quote = "Whatever you are, be a good one."
+print(pres, "once said," "\"",quote,"\".")
+
+
