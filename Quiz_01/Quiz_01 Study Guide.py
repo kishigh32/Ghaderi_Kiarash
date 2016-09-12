e98accae8ea79339bb8fd3fc5e6489ea545ecd3f
kishigh32 <kishigh32@gmail.com>
2016-09-12 10:25:14 -0700

    prac

--- Quiz_01/Quiz_01 Study Guide.py
+++ Quiz_01/Quiz_01 Study Guide.py
@@ -5,4 +5,35 @@ pres = "Abraham Lincoln"
 quote = "Whatever you are, be a good one."
 print(pres, "once said," "\"",quote,"\".")
 
+h = 45
+w = 64
+print("The area of your rectangle is..." ,h*w)
 
+slices = int(input("How many pieces of pizza do you want?"))
+print(slices, "slices, coming right up!")
+print("\"Seven\" does not work, because we put \"int\". This means the system only accepts integer numbers.")
+
+name = input("What is your name? " )
+print("Is there a Mr. or Mrs. " + name + "?")
+print("It would prints the option C.")
+
+answer1 = input ("How many siblings do you have? ")
+answer2 = input("What are their names? ")
+answer3 = input("OK, which one is your favorite?")
+print("Awe! " + answer3 + " is your favorite.");
+print("It would print none of the options.")
+
+print("R. Kelly said \"I believe I can fly!\"")
+print("We need to put backslash before the \" of the phrase that we are trying to quote")
+
+print("This one goes on top \nThis one goes on bottom")
+
+print("a\tb\tc\nd\te\tf\ng\th\ti")
+
+math = float (input("Please enter Math grade: ")
+science = float (input("Please enter Science grade: ")
+english = float (input("Please enter English grade: ")
+history = float (input("Please enter History grade: ")
+chemistry = float (input("Please enter Chemistry grade: ")
+		
+gpa = (math + science + english + history + chemistry)/500
