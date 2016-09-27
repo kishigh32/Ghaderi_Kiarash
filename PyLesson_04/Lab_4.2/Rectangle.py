429fe9495c1b6bc3242508e11538fb7e04d66a01
kishigh32 <kishigh32@gmail.com>
2016-09-27 10:23:24 -0700

    gf

---
+++ PyLesson_04/Lab_4.2/Rectangle.py
@@ -0,0 +1,16 @@
+
+
+def format(perimeter):
+    output = "{:0.5f}".format(perimeter)
+    return output
+
+
+length = float(input("Enter the length of the rectangle in feet:"))
+width = float(input("Enter the width of the rectangle in feet:"))
+perimeter = 2 * (length + width)
+
+
+print("Your rectangle is" ,format(perimeter), "sq ft around.")
+    
+    
+
