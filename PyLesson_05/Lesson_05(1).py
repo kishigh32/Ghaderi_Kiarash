d93e10ffceb4a4484f3f8c3998adf4818614e9ec
kishigh32 <kishigh32@gmail.com>
2016-10-06 10:24:11 -0700

    les 05

---
+++ PyLesson_05/Lesson_05(1).py
@@ -0,0 +1,16 @@
+num = 5
+
+if num == 5:
+    print("num equals 5!")
+
+if num == 3:
+    print("num equals 3!")
+
+
+word1 = input("Please enter your word:")
+word2 = "handsome"
+
+if word1 == word2:
+    print("they are the same!")
+if word1 != word2:
+    print("mismatch!")
