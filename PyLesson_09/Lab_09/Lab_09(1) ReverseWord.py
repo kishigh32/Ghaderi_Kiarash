b156a9aba40e91f9027eaa2c8a39bea3311e5aa9
kishigh32 <kishigh32@gmail.com>
2016-12-01 10:25:38 -0800

    9.1 started

---
+++ PyLesson_09/Lab_09/Lab_09(1) ReverseWord.py
@@ -0,0 +1,13 @@
+words = ["I", "am", "Kiarash", "Ghaderi", "KG"]
+
+output = ""
+j = 0
+
+for i in words:
+    output += i + " "
+
+print("In order:", output)
+print()
+
+for k in range(len(words), 0, -1):
+    
---
+++ PyLesson_09/Lesson_09(3).py
@@ -0,0 +1,11 @@
+word = "P r o f e s s o r"
+myList = word.split(" ")
+
+output = ""
+
+for i in myList:
+    output += (i)
+
+
+print(output)
+
