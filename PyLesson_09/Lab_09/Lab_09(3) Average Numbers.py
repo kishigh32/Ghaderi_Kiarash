07ac053deb47784128f916e260507a195be24f2f
kishigh32 <kishigh32@gmail.com>
2016-12-08 10:24:09 -0800

    KoG

--- PyLesson_09/Lab_09/Lab_09(3) Average Numbers.py
+++ PyLesson_09/Lab_09/Lab_09(3) Average Numbers.py
@@ -2,8 +2,12 @@ numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 print("Numbers...")
 
-output = ""
-for i in numbers:
-    output += i + " "
+
+
 
 print(output)
+print()
+print("The average of the above number is..." + average(numbers))
+
+def average(numbers):
+    
---
+++ PyLesson_09/Lab_09/Lab_09(4) FibonacciSequence.py
@@ -0,0 +1,3 @@
+num1 = int(input("Enter your starting number please: "))
+num2 = int(input("Enter your sequence size please: "))
+
