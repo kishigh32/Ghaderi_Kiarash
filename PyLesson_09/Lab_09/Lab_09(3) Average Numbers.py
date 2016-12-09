58d2a0f0a5d6c3eb0c75bb9ed75cdd5ac07694f4
kishigh32 <kishigh32@gmail.com>
2016-12-09 10:22:59 -0800

    touching 9.3 + 9.5

--- PyLesson_09/Lab_09/Lab_09(3) Average Numbers.py
+++ PyLesson_09/Lab_09/Lab_09(3) Average Numbers.py
@@ -1,6 +1,12 @@
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+for i in range(0, 10):
+    i = random.randint(1, 100)
+
 print("Numbers...")
+
+output = ""
+
 
 
 
@@ -10,4 +16,6 @@ print()
 print("The average of the above number is..." + average(numbers))
 
 def average(numbers):
+    avg = 0
+    
     
--- PyLesson_09/Lab_09/Lab_09(4) FibonacciSequence.py
+++ PyLesson_09/Lab_09/Lab_09(4) FibonacciSequence.py
@@ -1,3 +1,13 @@
-num1 = int(input("Enter your starting number please: "))
-num2 = int(input("Enter your sequence size please: "))
+num = int(input("Enter your starting number please: "))
+size = int(input("Enter your sequence size please: "))
+
+seq = []
+
+
+for i in range(0, size):
+    if i == 1 or i == 0:
+        seq.append(num)
+    else:
+        seq.append(seq[i-1] + seq[i-2])
+    print(seq[i], " ")
 
