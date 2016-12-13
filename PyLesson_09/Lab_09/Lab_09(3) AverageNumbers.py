e4e3683e5cf99c80d512cde672af1d7ecd054aa8
kishigh32 <kishigh32@gmail.com>
2016-12-13 10:22:59 -0800

    9.6 left

---
+++ PyLesson_09/Lab_09/Lab_09(3) AverageNumbers.py
@@ -0,0 +1,25 @@
+numbers = []
+import random
+
+for i in range(0, 10):
+    numbers.append(random.randint(1, 100))
+
+print("Numbers...")
+output = ""
+
+for i in numbers:
+    output += str(i) + " "
+
+
+print(output)
+print()
+
+def average(nums):
+    sum = 0
+    for i in nums:
+        sum += i
+    return sum/len(nums)
+
+
+print("The average of above numbers is...", average(numbers))
+
--- PyLesson_09/Lab_09/Lab_09(5) KeepComposites.py
+++ PyLesson_09/Lab_09/Lab_09(5) KeepComposites.py
@@ -1,16 +1,18 @@
 nums = [2, 6, 8, 9, 10, 12, 13, 15, 17, 24, 55, 66, 78, 77, 79]
 
-
-def gFactor(nums):
-    for i in range(2, nums):
-        if nums % i ==0:
+print(nums)
+def gFactor(number):
+    for i in range(2, number):
+        if number % i == 0:
             return 1
-        else:
-            return 0
+    return 0
 
 def removePrimes():
-    
-        if gFactor(nums) == 0:
+    for number in nums:
+        if gFactor(number) == 0:
+            nums.remove(number)
             
-    
+removePrimes()
+
+print(nums)
 
