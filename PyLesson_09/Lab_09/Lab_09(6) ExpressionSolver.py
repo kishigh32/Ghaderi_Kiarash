96d0b1d546eef70b2c4a900ce2ba4d81a7fe47b4
kishigh32 <kishigh32@gmail.com>
2016-12-16 10:20:30 -0800

    done with 9

--- PyLesson_09/Lab_09/Lab_09(6) ExpressionSolver.py
+++ PyLesson_09/Lab_09/Lab_09(6) ExpressionSolver.py
@@ -1,28 +1,32 @@
-equation = input("Enter a mathematical expression please: ")
-
-
-
-while i < len(equation):
-    if i < len(equation) and (equation.append("*" or "/")):
-        if equation.append("*"):
-            equation.append(int(i-1) * int(i+1))
-        else:
-            equation.append(int(i-1) / int(i+1))
-        equation.remove(i-1)
-        equation.remove(i)
-    i += 1
-
-i = 0
-
-while i < len(equation):
-    if i < len(equation) and (equation.append("+" or "-")):
-        if equation.append("+"):
-            equation.append(int(i-1) + int(i+1))
-        else:
-            equation.append(int(i-1) - int(i+1))
-        equation.remove(i-1)
-        equation.remove(i)
-    i += 1
+expression = input("Enter a mathematical expression please: ")
+equation = expression.split(" ")
 
 print(equation)
+
+i = 0
+while i < len(equation):
+    if i < len(equation) and (equation[i] == "*" or equation[i] == "/"):
+        if equation[i] == "*":
+            equation[i] = int(equation[i-1]) * int(equation[i+1])
+        else:
+            equation[i] = int(equation[i-1]) / int(equation[i+1])
+        equation.pop(i-1)
+        equation.pop(i)
+    else:
+        i += 1
+
+i = 0
+while i < len(equation):
+    if i < len(equation) and (equation[i] == "+" or equation[i] == "-"):
+        if equation[i] == "+":
+            equation[i] = int(equation[i-1]) + int(equation[i+1])
+        else:
+            equation[i] = int(equation[i-1]) - int(equation[i+1])
+        equation.pop(i-1)
+        equation.pop(i)
+    else:
+        i += 1
+
+print(equation)
+
 
